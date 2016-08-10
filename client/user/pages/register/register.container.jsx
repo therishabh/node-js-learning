f205e8b974a1aa4dda879683efa5dc8e63f9081e
Abhijit <latinojeet@gmail.com>
2016-08-10 11:36:30 +0530

    react structure

---
+++ client/user/pages/register/register.container.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class RegisterPage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>GMA Register</h1>
+      </div>
+    );
+  }
+}
+
+export default RegisterPage;
