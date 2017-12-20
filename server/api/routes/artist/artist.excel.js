cd56aff1053946f2348304c8ff2bdc206ca4019f
Rishabh Agrawal <therishabhagrawal@gmail.com>
2017-12-20 15:28:09 +0530

    first commit

--- server/api/routes/artist/artist.excel.js
+++ server/api/routes/artist/artist.excel.js
@@ -9,7 +9,7 @@ var Flickr = require("flickrapi"),
         api_key: "2fb9e85f1290cdd98114f6026cb08213",
         secret: "0f517211adedba0b",
         user_id: "143739185@N03",
-        access_token: "72157671477889850-374a3233bcb19ff3",
+        access_token: "72157675787824590-d704b169962cbe0d",
         access_token_secret: "e120b483a3b9f5d8",
         permissions: "write"
     };
